b2ea014cd3b9cd5afaa5f54ad9d69b1b56f4d8a5
Teddy Knox <teddyknox@gmail.com>
2013-04-09 15:27:33 -0400

    Done with first iteration of simple k-means

--- kmeans.py
+++ kmeans.py
@@ -29,8 +29,20 @@ class KMeans:
     clustering does NOT begin until the cluster method is called.
     """
 
-    #########
-    # TODO: Add your code to initialize the K-means cluster
+    cls = self.clusters = []
+    cens = self.centers = []
+
+    cluster_indexes = []
+    num_points = len(data)
+    possible_indexes = range(num_points)
+
+    for i in range(k):
+      cls.append([])
+      which_index = randint(0, num_points-i)
+      cens.append(data[possible_indexes[which_index]])
+      possible_indexes.remove(which_index)
+
+    cls[0] = data
 
   def cluster(self, iterations=INFINITY):
     """
@@ -40,23 +52,46 @@ class KMeans:
     i.e. don't change. Optionally, you can specify a fixed number of iterations.
     """
 
-    #########
-    # TODO: code to run k-means
+    for i in xrange(iterations):
+      self.assign_to_centers()
+      self.recalculate_centers()
 
   def assign_to_centers(self):
     """
     Assign the points to the cluster centers.  This will
     update the clusters.
     """
-    #########
-    # TODO: code to reassign data points
+
+    points = sum(self.clusters, [])
+    new_clusters = [[] for x in range(len(self.clusters))] # blank clusters
+
+    for point in points:
+      min_dist = INFINITY
+      min_k = -1
+      for x, center in enumerate(self.centers):
+        dist = center.euclidean(point)
+        if dist < min_dist:
+          min_dist = dist
+          min_k = x
+      new_clusters[min_k].append(point)
+    self.clusters = new_clusters 
 
   def recalculate_centers(self):
     """
     Recalculte the cluster centers based on the current clusters.
     """
-    #########
-    # TODO: code to recalculate centers
+    new_centers = []
+    for cluster in clusters:
+      dim_totals = {}
+      for point in cluster:
+        for key in point:
+          if key in dim_totals:
+            dim_totals[key] += point[key]
+          else:
+            dim_totals[key] = point[key]
+        for key in dim_total:
+          dim_total[key] = float(dim_total[key])/len(cluster)
+      new_centers.append(DataPoint(dim_total))
 
 
   def get_clusters(self):
@@ -67,12 +102,10 @@ class KMeans:
     a list of clusters.
     """
 
-    #########
-    # TODO
+    return self.clusters
 
 
   def get_centers(self):
     """ Return the current cluster centers as a list of DataPoints. """
 
-    #########
-    # TODO
+    return self.centers
