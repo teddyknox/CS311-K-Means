bfc3779c766878cddd7fe32156cb2e2fdb0f4197
Will Potter <will@wpotter.me>
2013-04-11 18:31:15 -0400

    added an updated eval function

--- eval.py
+++ eval.py
@@ -9,17 +9,19 @@ for label, tokens in reader:
 	# pass the label to the WordData constructor to keep track of it for later use
 	data.append(WordDataPoint(tokens, label=label))
 # k=five clusters
-clusterer = KMeans(data, 5, KMeans.COSINE)
-clusters = clusterer.cluster()
+clusterer = KMeans(data, 5, DataPoint.EUCLIDEAN)
+clusterer.cluster(4)
+print "---KMEANS DONE---"
+clusters = clusterer.get_clusters()	
 
 # Store purities relative to clusters
 purities = []
 for cluster in clusters:
 	purity_labels = {}
 
-	print "-" * 25
+	# print "-" * 25
 	for point in cluster:
-		print point.get_label() + ": " + str(point)
+		# print point.get_label() + ": " + str(point)
 		if point.get_label() in purity_labels:
 			purity_labels[point.get_label()] += 1
 		else:
@@ -30,14 +32,37 @@ for cluster in clusters:
 		if v > max_occurence:
 			max_occurence = v
 			max_label = k
-	purities.append(max_occurence / len(cluster))
+	purities.append(max_occurence / float(len(cluster)))
 
 
 
 i = 0
-# Print individual purities
+# Print average purities purities
+print "-"*25
+print "NON-WEIGHTED PURITY"
+print "-"*25
 for p in purities:
 	print "Cluster "+str(i)+":"+str(p)
-print "Average purity: "+str(sum(l) / float(len(l)))
+	i+=1
 
+print "Average Unweighted Purity: "+str(sum(purities) / float(len(purities)))
+
+print ""
+print "-"*25
+print "WEIGHTED PURITY"
+print "-"*25
+total_points = 0
+cluster_lengths=[]
+for cluster in clusters:
+	c = len(cluster)
+	total_points += c
+	cluster_lengths.append(c)
+
+i=0
+weighted_purities = []
+for p in purities:
+	weighted_purities.append(p*cluster_lengths[i])
+	print "Cluster "+str(i)+":"+str(p*cluster_lengths[i])
+	i+=1
+print "Average Weighted Purity: "+str(sum(weighted_purities)/float(total_points))
 
